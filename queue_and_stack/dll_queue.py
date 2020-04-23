3bdb75697bc366c4b54e946a0e7b4df5190e6b42
Nicole Adams <nicoleadams.boston@gmail.com>
2020-04-23 10:16:42 -0400

    queue, stack, and lru cache

--- queue_and_stack/dll_queue.py
+++ queue_and_stack/dll_queue.py
@@ -2,18 +2,25 @@ import sys
 sys.path.append('../doubly_linked_list')
 from doubly_linked_list import DoublyLinkedList
 
-
+### Queues
+#  * Should have the methods: `enqueue`, `dequeue`, and `len`.
+  
 class Queue:
     def __init__(self):
-        self.size = 0
         # Why is our DLL a good choice to store our elements?
-        # self.storage = ?
+        self.size = 0
+        self.storage = DoublyLinkedList()
 
+#  * `enqueue` should add an item to the back of the queue.
     def enqueue(self, value):
-        pass
+        self.size
+        return self.storage.add_to_tail(value)
 
+#  * `dequeue` should remove and return an item from the front of the queue.
     def dequeue(self):
-        pass
-
+        return self.storage.remove_from_head()
+        
+# * `len` returns the number of items in the queue.
     def len(self):
-        pass
+        return self.storage.length
+
